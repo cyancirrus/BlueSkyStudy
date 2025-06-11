8fdba21f51ce142b1e48df7f390bde332660e81a
autumn <cyancirrus@gmail.com>
2025-06-11 15:10:29 -0500

    easier testing of endpoints

--- dev.sh
+++ dev.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+cargo run &
+SERVER_PID=$!
+start_server() {
+	until curl -s -o /dev/null 0.0.0.0:3000; do
+		echo "waiting for server to start"
+		sleep 0.5
+	done
+	echo "server started testing endpoints"
+}
+
 application_home() {
 	echo $(curl 0.0.0.0:3000)
 }
 
+start_server
 application_home
+
+kill $SERVER_PID
